a9e3d67c1746352a177b0c68903e6694b2da7af1
Nguyen Van Dat <nvdatdev@gmail.com>
2023-05-21 20:59:24 +0700

    Updates

---
+++ precache-manifest.477977a708c3ccee4d97e72c407a48d9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "59830135bcf833c414ab",
+    "url": "/nguyendat01qb/information/static/css/main.56d17c6c.chunk.css"
+  },
+  {
+    "revision": "59830135bcf833c414ab",
+    "url": "/nguyendat01qb/information/static/js/main.59830135.chunk.js"
+  },
+  {
+    "revision": "b7c107a9b040005b4755",
+    "url": "/nguyendat01qb/information/static/js/1.b7c107a9.chunk.js"
+  },
+  {
+    "revision": "6dec81eda205f48cbe62",
+    "url": "/nguyendat01qb/information/static/js/2.6dec81ed.chunk.js"
+  },
+  {
+    "revision": "d24902a7f4d4b31614ec",
+    "url": "/nguyendat01qb/information/static/js/runtime~main.d24902a7.js"
+  },
+  {
+    "revision": "8e55b65e320587226263daf323c2b4d1",
+    "url": "/nguyendat01qb/information/static/media/20472940a42f6b71323e.8e55b65e.jpg"
+  },
+  {
+    "revision": "69b19dceca4becf5de6b6bcac73ba2e0",
+    "url": "/nguyendat01qb/information/static/media/274605957_1233385117413357_8756173668328546270_n.69b19dce.jpg"
+  },
+  {
+    "revision": "27fb86eec61fe63e7e6e0374cb4cfe62",
+    "url": "/nguyendat01qb/information/static/media/273036052_1317991808666420_3084454343553392006_n.27fb86ee.jpg"
+  },
+  {
+    "revision": "e4a129eaa3bac4ac8a37336d14419d3e",
+    "url": "/nguyendat01qb/information/static/media/271274899_3147560795513879_5434809409132509196_n.e4a129ea.jpg"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/nguyendat01qb/information/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/nguyendat01qb/information/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/nguyendat01qb/information/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/nguyendat01qb/information/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/nguyendat01qb/information/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/nguyendat01qb/information/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/nguyendat01qb/information/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "bc5bd7701cdbfd5160223eacbe42c7fe",
+    "url": "/nguyendat01qb/information/static/media/a12dd435595a9604cf4b.bc5bd770.jpg"
+  },
+  {
+    "revision": "deace2279f7bc04433b1963d5c195c46",
+    "url": "/nguyendat01qb/information/static/media/bg-1.deace227.jpg"
+  },
+  {
+    "revision": "1adef7642d3375197c0809d972c18ac1",
+    "url": "/nguyendat01qb/information/static/media/bg.1adef764.jpg"
+  },
+  {
+    "revision": "854a96f40528ee318ff127234da011f4",
+    "url": "/nguyendat01qb/information/index.html"
+  }
+];
